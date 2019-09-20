3366f4a7a0f3a5aa9fd1c7474753958301c42b1e
Dec-F <dec_fan@icloud.com>
2019-09-21 01:10:17 +0800

    fix(move):fix error

--- src/colloct.js
+++ src/colloct.js
@@ -43,7 +43,8 @@ const rebuild = (dir, files) => Promise.all(
           resolve(e);
         });
     });
-    return Promise.all([mp(), move(file, `${p}/${path.basename(file)}`)]);
+    const p2 = () => (file === `${p}/${path.basename(file)}` ? Promise.resolve() : move(file, `${p}/${path.basename(file)}`));
+    return Promise.all([mp(), p2()]);
   }),
 ).then(() => dir);
 function flat(arr, depth) {

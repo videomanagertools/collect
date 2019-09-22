393bd7d73054c0cce993343e5a11fd9b0224ab58
Dec-F <dec_fan@icloud.com>
2019-09-22 13:53:24 +0800

    feat(entry):add opt

--- src/method.js
+++ src/method.js
@@ -1,6 +1,6 @@
 import { collect, rebuild } from './colloct';
 
-export default function CRD(filepath) {
-  return collect(filepath).then(files => rebuild(filepath, files).then(res => collect(res)));
+export default function CRD(filepath, opt) {
+  return collect(filepath, opt).then(files => rebuild(filepath, files).then(res => collect(res)));
 }
 export { collect, rebuild } from './colloct';
